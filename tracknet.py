9acb23b3b1231de133d4a8ab4b690d47958233a2
Bartek <pony830906@gmail.com>
2023-10-02 22:12:26 +0800

    fix: calculate offset_x, offset_y

--- tracknet.py
+++ tracknet.py
@@ -90,8 +90,8 @@ class TrackNetLoss:
 def targetGrid(target_x, target_y, stride):
     grid_x = int(target_x / stride)
     grid_y = int(target_y / stride)
-    offset_x = (target_x / stride) - grid_x
-    offset_y = (target_y / stride) - grid_y
+    offset_x = (target_x % stride)
+    offset_y = (target_y % stride)
     return grid_x, grid_y, offset_x, offset_y
 
 def focal_loss(pred_logits, targets, alpha=0.95, gamma=4.0, epsilon=1e-6, weight=10):
