80e02bd7a49aae332806dfeb781648c493861e4f
Bartek <pony830906@gmail.com>
2023-10-25 21:11:03 +0800

    refactor yolo: add test log

--- ultralytics/yolo/engine/trainer.py
+++ ultralytics/yolo/engine/trainer.py
@@ -331,6 +331,9 @@ class BaseTrainer:
                 with torch.cuda.amp.autocast(self.amp):
                     batch = self.preprocess_batch(batch)
                     self.loss, self.loss_items = self.model(batch)
+                    if self.tloss is not None:
+                        LOGGER.info("self.tloss is not None")
+                    print(self.loss_items + "\n")
                     if RANK != -1:
                         self.loss *= world_size
                     self.tloss = (self.tloss * i + self.loss_items) / (i + 1) if self.tloss is not None \
