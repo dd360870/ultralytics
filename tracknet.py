a611e96d023dc9cfdb27836f18753600ce5a16c9
Bartek <pony830906@gmail.com>
2023-12-06 23:38:59 +0800

    test: change mean

--- tracknet.py
+++ tracknet.py
@@ -189,9 +189,9 @@ def custom_loss(y_true, y_pred, class_weight):
 
     loss = (-1) * class_weights * custom_weights * (y_true * torch.log(torch.clamp(y_pred, min=torch.finfo(y_pred.dtype).eps, max=1)) + 
                                                     (1 - y_true) * torch.log(torch.clamp(1 - y_pred, min=torch.finfo(y_pred.dtype).eps, max=1)))
-    penalty = (y_true * (1 - y_pred) * 100)
+    # penalty = (y_true * (1 - y_pred) * 1000)
 
-    return torch.mean(loss+penalty)
+    return torch.sum(loss * y_true) / torch.sum(y_true) + torch.mean(loss * (1 - y_true))
 
 def focal_loss(pred_logits, targets, alpha=0.95, gamma=2.0, epsilon=1e-3, weight=10):
     """
