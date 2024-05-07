4a0d427c5cb9b53265dd157cab0f31d9df943e31
Marvin <marvin.cs12@nycu.edu.tw>
2024-05-07 14:04:10 +0000

    Update tracknet dataset using cache

--- ultralytics/tracknet/dataset.py
+++ ultralytics/tracknet/dataset.py
@@ -1,106 +1,156 @@
 from matplotlib import patches, pyplot as plt
 import torch
 from torch.utils.data import Dataset
-from PIL import Image
+import cv2
+import hashlib
 import os
 import pandas as pd
 import numpy as np
 from torchvision import transforms
 import torch.nn.functional as F
+from tqdm import tqdm
+from functools import lru_cache
+from glob import glob
 
 class TrackNetDataset(Dataset):
-    def __init__(self, root_dir, num_input=10, transform=None):
+    def __init__(self, root_dir, num_input=10, transform=None, prefix=''):
+
         self.root_dir = root_dir
         self.transform = transform
         self.num_input = num_input
         self.samples = []
+        self.prefix = prefix
 
+        self.idx = set()
+
+        image_count = len(glob(os.path.join(self.root_dir, "*/", "frame/", "*/", "*.png")))
+
+        self.pbar = tqdm(total=image_count, miniters=1, smoothing=1)
         # Traverse all matches
-        for match_dir in os.listdir(root_dir):
-            match_dir_path = os.path.join(root_dir, match_dir)
+        for match_name in glob("*/", root_dir=root_dir):
+            match_name = match_name.strip('/')
+
+            match_dir_path = os.path.join(root_dir, match_name)
             
             # Check if it is a match directory
             if not os.path.isdir(match_dir_path):
                 continue
+
+            self.read_match(match_name)
+        self.pbar.close()
+
+    def read_match(self, match_name):
+        video_dir = os.path.join(self.root_dir, match_name, 'video')
+        csv_dir = os.path.join(self.root_dir, match_name, 'csv')
+
+        self.pbar.set_description(f'{self.prefix} Generating image cache: {match_name}/ ')
+
+        # Traverse all videos in the match directory
+        for video_name in glob("*.mp4", root_dir=video_dir):
+            video_name = video_name.removesuffix('.mp4')
+
+            csv_file = os.path.join(csv_dir, video_name + "_ball" + '.csv')
             
-            video_dir = os.path.join(match_dir_path, 'video')
-            ball_trajectory_dir = os.path.join(match_dir_path, 'csv')
+            ball_trajectory_df = self.__preprocess_csv(csv_file)
 
-            # Traverse all videos in the match directory
-            for video_file in os.listdir(video_dir):
-                # Get the video file name (without extension)
-                video_file_name = os.path.splitext(video_file)[0]
+            #print(ball_trajectory_df.columns)
+            #['Frame', 'Visibility', 'X', 'Y', 'dX', 'dY', 'hit']
 
-                # The ball_trajectory csv file has the same name as the video file
-                ball_trajectory_file = os.path.join(ball_trajectory_dir, video_file_name + "_ball" + '.csv')
-                
-                # Read the ball_trajectory csv file
-                ball_trajectory_df = pd.read_csv(ball_trajectory_file)
-                ball_trajectory_df['dX'] = -1*ball_trajectory_df['X'].diff(-1).fillna(0)
-                ball_trajectory_df['dY'] = -1*ball_trajectory_df['Y'].diff(-1).fillna(0)
-                ball_trajectory_df['hit'] = ((ball_trajectory_df['Event'] == 1) | (ball_trajectory_df['Event'] == 2)).astype(int)
+            frame_dir = os.path.join(self.root_dir, match_name, 'frame', video_name)
 
-                ball_trajectory_df['prev_hit'] = ball_trajectory_df['hit'].shift(fill_value=0)
-                ball_trajectory_df['next_hit'] = ball_trajectory_df['hit'].shift(-1, fill_value=0)
-                ball_trajectory_df['hit'] = ball_trajectory_df[['hit', 'prev_hit', 'next_hit']].max(axis=1)
-                
-                ball_trajectory_df = ball_trajectory_df.drop(['Fast'], axis=1)
-                ball_trajectory_df = ball_trajectory_df.drop(['Event'], axis=1)
-                ball_trajectory_df = ball_trajectory_df.drop(['Z'], axis=1)
-                ball_trajectory_df = ball_trajectory_df.drop(['Shot'], axis=1)
-                ball_trajectory_df = ball_trajectory_df.drop(['player_X'], axis=1)
-                ball_trajectory_df = ball_trajectory_df.drop(['player_Y'], axis=1)
-                ball_trajectory_df = ball_trajectory_df.drop(['prev_hit'], axis=1)
-                ball_trajectory_df = ball_trajectory_df.drop(['next_hit'], axis=1)
+            img_files = sorted(glob("*.png", root_dir=frame_dir), key=lambda x: int(x.removesuffix(".png")))
 
-                # Get the directory for frames of this video
-                frame_dir = os.path.join(match_dir_path, 'frame', video_file_name)
+            # Create sliding windows of num_input frames
+            for i in range(len(img_files) - (self.num_input-1)):
+                self.pbar.update(1)
 
-                # Get all frame file names and sort them by frame ID
-                frame_files = sorted(os.listdir(frame_dir), key=lambda x: int(os.path.splitext(x)[0]))
+                frames = img_files[i: i + self.num_input]
 
-                # Create sliding windows of num_input frames
-                for i in range(len(frame_files) - (num_input-1)):
-                    frames = [os.path.join(frame_dir, frame) for frame in frame_files[i: i + num_input]]
-                    ball_trajectory = ball_trajectory_df.iloc[i: i + num_input].values
-                    # Avoid invalid data
-                    if len(frames) == num_input and len(ball_trajectory) == num_input:
-                        self.samples.append((frames, ball_trajectory))
-            # check label result
-            # idx = np.random.randint(50, 100)
-            # frames, ball_trajectory = self.samples[206]
-            # ball_trajectory = torch.from_numpy(ball_trajectory)
-            # # Load images and convert them to tensors
-            # images = [self.open_image(frame) for frame in frames]
+                target = ball_trajectory_df.iloc[i: i + self.num_input].values
+                target = self.transform_coordinates(target, 1280, 720)
 
-            # idx = np.random.randint(0, 9)
-            # i = images[idx]
-            # xy = [(ball_trajectory[idx][2].item(), ball_trajectory[idx][3].item())]
-            #self.display_image_with_coordinates(i, xy)
+                # Avoid invalid data
+                if len(frames) == self.num_input and len(target) == self.num_input:
+                    npy_path = self.img_cache_dir(match_name, video_name, frames)
+
+                    self.samples.append({
+                        "match_name": match_name,
+                        "video_name": video_name,
+                        "cache_npy": npy_path,
+                        "img_files": frames,
+                        "target": target
+                    })
+
+                    self.img_cache(match_name, video_name, frames, npy_path)
+
+            self.pbar.update(self.num_input-1)
+
+    def img_cache_dir(self, match_name, video_name, img_files):
+        s = '|'.join([match_name]+[video_name]+img_files)
+        filename = hashlib.sha1(s.encode('utf-8')).hexdigest()
+
+        if filename in self.idx:
+            raise Exception('DUP: '+filename)
+        self.idx.add(filename)
+
+        d = os.path.join(self.root_dir, ".cache", filename[:2], filename[2:4])
+        os.makedirs(d, exist_ok=True)
+        f = os.path.join(d, f"{filename}.npy")
+        return f
+
+    def img_cache(self, match_name, video_name, img_files, npy_path):
+
+        if os.path.isfile(npy_path):
+            return
+
+        # generate cache
+        images = [self.__preprocess_img(os.path.join(self.root_dir, match_name, 'frame', video_name, img_file)) for img_file in img_files]
+        img = np.concatenate(images, 0)
+
+        np.save(npy_path, img)
+
+    def get_image_cache(self, path):
+        try:
+            return np.load(path)
+        except Exception as e:
+            raise Exception("File corrupted: " + path)
+
+    def __preprocess_csv(self, csv_file):
+        # Read the ball_trajectory csv file
+        ball_trajectory_df = pd.read_csv(csv_file)
+        ball_trajectory_df['dX'] = -1*ball_trajectory_df['X'].diff(-1).fillna(0)
+        ball_trajectory_df['dY'] = -1*ball_trajectory_df['Y'].diff(-1).fillna(0)
+
+        if 'hit' in ball_trajectory_df.columns:
+            ball_trajectory_df['hit'] = ((ball_trajectory_df['Event'] == 1) | (ball_trajectory_df['Event'] == 2)).astype(int)
+        else:
+            ball_trajectory_df['hit'] = 0
+
+        #ball_trajectory_df['prev_hit'] = ball_trajectory_df['hit'].shift(fill_value=0)
+        #ball_trajectory_df['next_hit'] = ball_trajectory_df['hit'].shift(-1, fill_value=0)
+        #ball_trajectory_df['hit'] = ball_trajectory_df[['hit', 'prev_hit', 'next_hit']].max(axis=1)
+
+        drop_columns = ['Fast', 'Event', 'Z', 'Shot', 'player_X', 'player_Y', 'prev_hit', 'next_hit']
+        
+        ball_trajectory_df = ball_trajectory_df.drop(drop_columns, axis=1, errors='ignore')
+
+        return ball_trajectory_df
     
     def __len__(self):
         return len(self.samples)
 
     def __getitem__(self, idx):
-        frames, ball_trajectory = self.samples[idx]
-        ball_trajectory = torch.from_numpy(ball_trajectory)
-        ball_trajectory = self.transform_coordinates(ball_trajectory, 1280, 720)
+        d = self.samples[idx]
         # Load images and convert them to tensors
-        images = [self.open_image(frame) for frame in frames]
-        images = [self.pad_to_square(img, 0) for img in images]
 
-        # check resize image
-        # idx = np.random.randint(0, 10)
-        # i = images[idx]
-        # xy = [(ball_trajectory[idx][2].item(), ball_trajectory[idx][3].item())]
-        # self.display_image_with_coordinates(i, xy)
+        img = self.get_image_cache(d['cache_npy'])
 
-        images = torch.cat(images, 0)  # Concatenate along the channel dimension
-        # Convert ball_trajectory to tensor
-        
-        shape = images.shape
-        shape = ball_trajectory.shape
-        return {"img": images, "target": ball_trajectory}
+        img = torch.from_numpy(img)
+        target = torch.from_numpy(d['target'])
+
+        img_files = [f"{self.root_dir}/../{im}" for im in d['img_files']]
+
+        return {"img": img, "target": target, "img_files": img_files}
 
     def transform_coordinates(self, data, w, h, target_size=640):
         """
@@ -117,7 +167,7 @@ class TrackNetDataset(Dataset):
         """
         
         # Clone the data to ensure we don't modify the original tensor in-place
-        data_transformed = data.clone()
+        data_transformed = data
         
         # Determine padding
         max_dim = max(w, h)
@@ -157,36 +207,46 @@ class TrackNetDataset(Dataset):
         fig, ax = plt.subplots(1)
 
         # Display the image
-        ax.imshow(img_array)
+        ax.imshow(img_array, cmap='gray')
 
         # Plot each coordinate
         for (x, y) in coordinates:
             ax.scatter(x, y, s=50, c='red', marker='o')
-            # Optionally, you can also draw a small rectangle around each point
-            rect = patches.Rectangle((x-5, y-5), 10, 10, linewidth=1, edgecolor='red', facecolor='none')
-            ax.add_patch(rect)
+            ## Optionally, you can also draw a small rectangle around each point
+            #rect = patches.Rectangle((x-5, y-5), 10, 10, linewidth=1, edgecolor='red', facecolor='none')
+            #ax.add_patch(rect)
 
         plt.show()
 
+    @lru_cache(maxsize=10)
+    def __preprocess_img(self, path, pad_value=0):
+        img = self.open_image(path)
+        img = self.pad_to_square(img, pad_value)
+        img.resize((1, 640, 640))
+        return img
+
     def open_image(self, path):
+        """Open image file, convert to grayscale and resize to half size
+        """
+
         # Open the image file
-        img = Image.open(path).convert('L')
+        img = cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2GRAY)
         
         # Reduce the resolution to half
-        width, height = img.size
-        img = img.resize((width // 2, height // 2))
-        
-        # Convert the image to a tensor
-        return transforms.ToTensor()(img)
+        h, w = img.shape
 
-    def pad_to_square(self, img, pad_value):
-        c, h, w = img.shape
+        return cv2.resize(img, dsize=(w // 2, h // 2), interpolation=cv2.INTER_CUBIC)
+        
+    def pad_to_square(self, img, pad_value=0):
+        """Adjust 2D tensor to square by padding {pad_value}
+        """
+        h, w = img.shape
         dim_diff = np.abs(h - w)
         # (upper / left) padding and (lower / right) padding
         pad1, pad2 = dim_diff // 2, dim_diff - dim_diff // 2
         # Determine padding
-        pad = (0, 0, pad1, pad2) if h <= w else (pad1, pad2, 0, 0)
+        pad = (0, 0, pad1, pad2) if h > w else (pad1, pad2, 0, 0)
         # Add padding
-        img = F.pad(img, pad, "constant", value=pad_value)
+        img = cv2.copyMakeBorder(img, *pad, borderType=cv2.BORDER_CONSTANT, value=pad_value)
 
         return img
